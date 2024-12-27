01b1537bcabe2ac794b7aa4e417dc4e5647d8a99
shahariaz <shahariaz.info@gmail.com>
2024-12-27 07:26:15 +0600

    feat: Refector registerController and separate it into TokenService

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,37 +1,19 @@
-import fs from 'fs'
-import path from 'path'
 import { NextFunction, Response } from 'express'
 import httpResponse from '../util/httpResponse'
 import { RegisterUserRequest } from '../types/interface'
 import { UserService } from '../services/UserService'
 import { Logger } from 'winston'
 import { validationResult } from 'express-validator'
-import createHttpError from 'http-errors'
-import { config } from '../config/config'
-import { AppDataSource } from '../config/data-source'
-import { RefreshToken } from '../entity/RefreshToken'
 import { TokenService } from '../services/TokenService'
 
 export class Auth {
-    private privateKey: Buffer | null = null
-
     constructor(
         private userService: UserService,
         private logger: Logger,
         private tokenService: TokenService
     ) {
-        this.loadKeys()
         this.userService = userService
         this.logger = logger
-    }
-
-    private loadKeys() {
-        try {
-            this.privateKey = fs.readFileSync(path.join(__dirname, '../../certs/private.pem'))
-        } catch (err) {
-            this.logger.error('Error loading key files', err)
-            throw new Error('Key files not found')
-        }
     }
 
     public async register(req: RegisterUserRequest, res: Response, next: NextFunction) {
@@ -50,9 +32,6 @@ export class Auth {
 
             const user = await this.userService.createUser({ firstName, lastName, email, password, role })
             this.logger.info('User has been registered', { id: user.id })
-            if (!this.privateKey) {
-                throw createHttpError(500, 'Private key not available')
-            }
 
             const payload = {
                 sub: String(user.id),
@@ -60,15 +39,9 @@ export class Auth {
                 role: user.role
             }
 
-            const accessToken = this.tokenService.generateAccessToken(payload, this.privateKey)
-            // Persist The refresh Token
-            const MS_IN_A_MONTH = 1000 * 60 * 60 * 24 * 30
-            const refreshRepo = AppDataSource.getRepository(RefreshToken)
-            const newRefreshToken = await refreshRepo.save({
-                expiresAt: new Date(Date.now() + MS_IN_A_MONTH),
-                user
-            })
-            const refreshToken = this.tokenService.genarateRefreshToken(payload, String(config.SECRET_KEY), newRefreshToken.id.toString())
+            const accessToken = this.tokenService.generateAccessToken(payload)
+            const newRefreshToken = await this.tokenService.presistRefreshToken(user)
+            const refreshToken = this.tokenService.genarateRefreshToken({ ...payload, id: newRefreshToken.id })
 
             res.cookie('accessToken', accessToken, {
                 domain: 'localhost',
--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -1,20 +1,56 @@
-import jwt from 'jsonwebtoken'
+import fs from 'fs'
+import path from 'path'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { config } from '../config/config'
+import { AppDataSource } from '../config/data-source'
+import { RefreshToken } from '../entity/RefreshToken'
+import { User } from '../entity/User'
+import logger from '../config/logger'
+import createHttpError from 'http-errors'
 export class TokenService {
-    generateAccessToken(payload: Record<string, string>, privateKey: Buffer): string {
-        const acessToken = jwt.sign(payload, privateKey, {
+    private privateKey: Buffer | null = null
+    private loadKeys() {
+        try {
+            this.privateKey = fs.readFileSync(path.join(__dirname, '../../certs/private.pem'))
+        } catch (err) {
+            logger.error('Error loading key files', err)
+            throw new Error('Key files not found')
+        }
+    }
+    constructor() {
+        this.loadKeys()
+    }
+    generateAccessToken(payload: JwtPayload): string {
+        if (!this.privateKey) {
+            throw createHttpError(500, 'Private key not available')
+        }
+        const acessToken = jwt.sign(payload, this.privateKey, {
             algorithm: 'RS256',
             expiresIn: '1h',
             issuer: 'auth-service'
         })
         return acessToken
     }
-    genarateRefreshToken(payload: Record<string, string>, secretKey: string, newRefreshToken: string): string {
-        const refreshToken = jwt.sign(payload, secretKey, {
+    genarateRefreshToken(payload: JwtPayload): string {
+        const refreshToken = jwt.sign(payload, String(config.SECRET_KEY), {
             algorithm: 'HS256',
             expiresIn: '30d',
             issuer: 'auth-service',
-            jwtid: newRefreshToken
+            jwtid: String(payload.id)
         })
         return refreshToken
+    }
+    presistRefreshToken(user: User): Promise<RefreshToken> {
+        const store = async () => {
+            // Persist The refresh Token
+            const MS_IN_A_MONTH = 1000 * 60 * 60 * 24 * 30
+            const refreshRepo = AppDataSource.getRepository(RefreshToken)
+            const newRefreshToken = await refreshRepo.save({
+                expiresAt: new Date(Date.now() + MS_IN_A_MONTH),
+                user
+            })
+            return newRefreshToken
+        }
+        return store()
     }
 }
